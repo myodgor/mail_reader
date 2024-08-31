9122edad8c02e1bd98d60986ff3400ab5b0aa76f
myodgor <73735209+myodgor@users.noreply.github.com>
2024-08-31 17:33:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,8 @@ chat_id = "айди чата куда добавлен бот"
 send_attach = True #пересылка вложений, чтобы отключить нужен параметр False
 
 encoding="utf-8"
+
+db_name = "uid.db" #база данных с UID отправленных в Телеграм писем
+
+#Папки исключения (Корзина, спам)
+ex_folders = ("&BCEEPwQwBDw-", "Spam", "SPAM", "&BBoEPgRABDcEOAQ9BDA-", "Trash", "TRASH")
